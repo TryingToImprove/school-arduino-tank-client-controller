a2d089ef4fa6126fe5983eb3eab0c2a75b325c7e
TryingToImprove <privat.oliver.lassen@gmail.com>
2016-04-27 10:13:25 +0200

    Handle multiple connections and disconnects

--- ArduinoTank.Raspberry.TestServer/Program.cs
+++ ArduinoTank.Raspberry.TestServer/Program.cs
@@ -5,45 +5,88 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ArduinoTank.Raspberry.TestServer
 {
     internal class Program
     {
-        private static readonly List<TcpClient> Clients = new List<TcpClient>();
+        private static readonly object Locker = new object();
+        private static readonly List<ClientConnection> Clients = new List<ClientConnection>();
+        private static readonly Queue<Message> _messages = new Queue<Message>();
 
         private static void Main(string[] args)
         {
             var listner = new TcpListener(IPAddress.Parse("127.0.0.1"), 7050);
             listner.Start();
 
+            var writerThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    Message msg;
+                    List<ClientConnection> clients;
+
+                    lock (Locker)
+                    {
+                        if (!_messages.Any())
+                        {
+                            continue;
+                        }
+
+                        msg = _messages.Dequeue();
+                        clients = Clients.Where(x => x != msg.Sender).ToList();
+                    }
+
+                    if (msg != null)
+                    {
+                        foreach (var x in clients)
+                        {
+                            x.Writer.WriteLine(msg.Content);
+                            x.Writer.Flush();
+                        }
+                    }
+                }
+            });
+
+            writerThread.Start();
+
             while (true)
             {
                 var client = listner.AcceptTcpClient();
 
-                Clients.Add(client);
-
                 var childSocketThread = new Thread(new ParameterizedThreadStart(ClientConection));
                 childSocketThread.Start(client);
             }
-
-            listner.Stop();
         }
-
+        
         private static void ClientConection(object clientObj)
         {
-            var client = (TcpClient)clientObj;
-
-            var sr = new StreamReader(client.GetStream());
-            while (true)
+            using (var client = new ClientConnection((TcpClient)clientObj))
             {
-                var msg = sr.ReadLine();
-
-                foreach (var x in Clients.Where(x => x != client))
+                lock (Locker)
                 {
-                    var sw = new StreamWriter(x.GetStream());
-                    sw.WriteLine(msg);
-                    sw.Flush();
+                    Clients.Add(client);
+                }
+
+                while (true)
+                {
+                    var msg = client.Reader.ReadLine();
+
+                    if (msg == null)
+                    {
+                        break;
+                    }
+
+                    lock (Locker)
+                    {
+                        _messages.Enqueue(new Message { Content = msg, Sender = client });
+                    }
+                }
+
+                lock (Locker)
+                {
+                    Clients.Remove(client);
                 }
             }
         }
