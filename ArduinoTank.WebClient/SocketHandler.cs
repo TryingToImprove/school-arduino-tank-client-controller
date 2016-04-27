3db1ad25ab8c22b4adb04e92bf9201ccdc9f2537
TryingToImprove <privat.oliver.lassen@gmail.com>
2016-04-27 10:22:37 +0200

    Add error message

--- ArduinoTank.WebClient/SocketHandler.cs
+++ ArduinoTank.WebClient/SocketHandler.cs
@@ -6,15 +6,25 @@ using System.Net.Sockets;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 public class SocketHandler : WebSocketConnection
 {
     public override Task OnMessageReceived(ArraySegment<byte> message, WebSocketMessageType type)
     {
         var requestMessage = Encoding.UTF8.GetString(message.Array, message.Offset, message.Count);
-        
-        Communicator.Instance.Send(requestMessage);
 
+        try
+        {
+            Communicator.Instance.Send(requestMessage);
+        }
+        catch (Exception ex)
+        {
+            var msg = JsonConvert.SerializeObject(ex);
+
+            return SendText(Encoding.UTF8.GetBytes(msg), true);
+        }
+        
         return Task.FromResult(0);
     }
 }
